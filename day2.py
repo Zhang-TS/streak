4057b67b6cf1e76d37d631952c9418850c20cb90
yulu <yulu8798@gmail.com>
2013-11-26 11:23:22 +0800

    modify day2, finish bubble sort

--- day2.py
+++ day2.py
@@ -23,7 +23,7 @@ def insertion_sort(A):
 	for j in range(1, len(A)):
 		key = A[j]
 		i = j - 1
-		while i >= 0 and A[i] > key:
+		while i >= 0 and A[i] < key:
 			A[i+1] = A[i]
 			i = i - 1
 		A[i+1] = key
@@ -31,8 +31,32 @@ def insertion_sort(A):
 
 """
 Bubble sort:
+works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted. 
+psudocode:
+for i = 1:n
+	swapped = false
+	for j = n:i+1
+		if a[j] < a[j-1]
+			swap a[j, j-1]
+			swapped = true
+	break if not swapped
+end
+"""
+def bubble_sort(A):
+	while True:
+		swap = False
+		for i in range(1, len(A)):
+			if A[i]	< A[i-1]:
+				temp = A[i]
+				A[i] = A[i-1]
+				A[i-1] = temp
+				swap = True
+		if not swap:
+			break
 
 
 A = [3,6,2,8,12,4,88,3,1,9]
 sort = insertion_sort(A)
+bubble_sort(A)
 print sort
+print A
--- day4.py
+++ day4.py
@@ -10,6 +10,8 @@ quick-sort:a divide-and-conquer algorithm. It first divides a large list into tw
 2. reorder the list so that all elements with values less than the pivot come before the pivot, while all elements with values greater than the pivot come after it(equal values can go either way). After this partitioning, the pivot is in its final position. This is called the partition operation
 3. Recursively apply the above steps to the sub-list of elements with smaller values and separately the sub-list of elements with greater values
 
+psudocode:
+
 """
 #input list A, start index i, and end index j
 def partition(A, i, j):
@@ -27,13 +29,24 @@ def partition(A, i, j):
 def quick_sort(A, i, j):
 	size = j - i + 1;
 	q = partition(A, i, j)
-	print q
 	if q-i > 1:
 		quick_sort(A, i, q)
 	if j-q > 1:
 		quick_sort(A, q+1, j)
 
+
+#using list comprehensions:
+def qsort(list):
+	if not list:
+		return []
+	else:
+		pivot = list[0]
+		less = [x for x in list if x < pivot]
+		more = [x for x in list[1:] if x >= pivot]
+		return qsort(less) + [pivot] + qsort(more)
+
 unsort = [3, 4, 5, 23, 57, 1, 89, 63, 32, 83, 99]
-quick_sort(unsort,0, len(unsort)-1)
-print unsort
+#quick_sort(unsort,0, len(unsort)-1)
+sort = qsort(unsort)
+print sort
 
